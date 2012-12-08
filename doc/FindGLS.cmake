48e4467a700e89992e7fc40e458442e1c15b15e8
Martin Lambers <marlam@marlam.de>
2012-12-08 18:40:08 +0100

    Update FindGLS.cmake.

--- doc/FindGLS.cmake
+++ doc/FindGLS.cmake
@@ -1,56 +1,58 @@
-# - Find the GLS includes and library
+# - Try to find the GLS library (libgls)
 #
-# This module accepts the following environment variables:
-#  GLS_ROOT - Specify the location of libgls
+# Once done this will define
 #
-# This module defines
-#  GLS_FOUND - If false, do not try to use libgls.
-#  GLS_INCLUDE_DIRS - Where to find the headers.
-#  GLS_LIBRARIES - The libraries to link against to use libgls.
+#  GLS_FOUND - System has libgls
+#  GLS_INCLUDE_DIR - The libgls include directory
+#  GLS_LIBRARIES - The libraries needed to use libgls
 
-# Copyright (C) 2012
-# Martin Lambers <marlam@marlam.de>
+# Adapted from FindGnuTLS.cmake 2012-12-06, Martin Lambers.
+# Original copyright notice:
+#=============================================================================
+# Copyright 2009 Kitware, Inc.
+# Copyright 2009 Philip Lowman <philip@yhbt.com>
+# Copyright 2009 Brad Hards <bradh@kde.org>
+# Copyright 2006 Alexander Neundorf <neundorf@kde.org>
 #
-# Copying and distribution of this file, with or without modification, are
-# permitted in any medium without royalty provided the copyright notice and this
-# notice are preserved. This file is offered as-is, without any warranty.
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
 
-INCLUDE(FindPkgConfig OPTIONAL)
 
+IF(GLS_INCLUDE_DIR AND GLS_LIBRARY)
+    # in cache already
+    SET(GLS_FIND_QUIETLY TRUE)
+ENDIF(GLS_INCLUDE_DIR AND GLS_LIBRARY)
+
+FIND_PACKAGE(PkgConfig QUIET)
 IF(PKG_CONFIG_FOUND)
-    INCLUDE(FindPkgConfig)
-    PKG_CHECK_MODULES(GLS gls)
-ELSE(PKG_CONFIG_FOUND)
-    FIND_PATH(GLS_INCLUDE_DIRS gls/gls.h
-        $ENV{GLS_ROOT}/include
-        $ENV{GLS_ROOT}
-        ~/Library/Frameworks
-        /Library/Frameworks
-        /usr/local/include
-        /usr/include
-        /sw/include # Fink
-        /opt/local/include # DarwinPorts
-        /opt/csw/include # Blastwave
-        /opt/include
-        /usr/freeware/include
-    )
-    FIND_LIBRARY(GLS_LIBRARIES 
-        NAMES gls libgls
-        PATHS
-        $ENV{GLS_ROOT}/lib
-        $ENV{GLS_ROOT}
-        ~/Library/Frameworks
-        /Library/Frameworks
-        /usr/local/lib
-        /usr/lib
-        /sw/lib
-        /opt/local/lib
-        /opt/csw/lib
-        /opt/lib
-        /usr/freeware/lib64
-    )
-    SET(GLS_FOUND "NO")
-    IF(GLS_LIBRARIES AND GLS_INCLUDE_DIRS)
-        SET(GLS_FOUND "YES")
-    ENDIF(GLS_LIBRARIES AND GLS_INCLUDE_DIRS)
-ENDIF(PKG_CONFIG_FOUND)
+    # try using pkg-config to get the directories and then use these values
+    # in the FIND_PATH() and FIND_LIBRARY() calls
+    PKG_CHECK_MODULES(PC_GLS QUIET gls)
+    SET(GLS_VERSION_STRING ${PC_GLS_VERSION})
+ENDIF()
+
+FIND_PATH(GLS_INCLUDE_DIR gls/gls.h HINTS ${PC_GLS_INCLUDE_DIRS})
+
+FIND_LIBRARY(GLS_LIBRARY NAMES gls libgls HINTS ${PC_GLS_LIBRARY_DIRS})
+
+MARK_AS_ADVANCED(GLS_INCLUDE_DIR GLS_LIBRARY)
+
+# handle the QUIETLY and REQUIRED arguments and set GLS_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GLS
+    REQUIRED_VARS GLS_LIBRARY GLS_INCLUDE_DIR
+    VERSION_VAR GLS_VERSION_STRING
+)
+
+IF(GLS_FOUND)
+    SET(GLS_LIBRARIES ${GLS_LIBRARY})
+    SET(GLS_INCLUDE_DIRS ${GLS_INCLUDE_DIR})
+ENDIF()
